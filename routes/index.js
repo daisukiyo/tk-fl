13d7018a4146735df062a4418e41af1f7c37ae08
daisukiyo <tk.rain09@gmail.com>
2019-03-05 22:51:28 -0800

    add landing page to services

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,20 @@ const Service = require('../models/service');
 
 module.exports = (app) => {
 
-  /* GET home page. */
   app.get('/', (req, res) => {
-    const page = req.query.page || 1
+    res.render('services-lp');
+  })
+  // /* GET home page. */
+  // app.get('/services', (req, res) => {
+  //   const page = req.query.page || 1
 
-    Service.paginate({}, {page: page}).then((results) => {
-        if (req.header('content-type') == 'application/json') {
-            return res.json({ services: services, categories: categories });
-        } else {
-            return res.render('services-index', { services: results.docs, pagesCount: results.pages, currentPage: page });
-        }
+  //   Service.paginate({}, {page: page}).then((results) => {
+  //       if (req.header('content-type') == 'application/json') {
+  //           return res.json({ services: services, categories: categories });
+  //       } else {
+  //           return res.render('services-index', { services: results.docs, pagesCount: results.pages, currentPage: page });
+  //       }
 
-    });
-  });
+  //   });
+  // });
 }
--- routes/services.js
+++ routes/services.js
@@ -40,6 +40,20 @@ const client = new Upload(process.env.S3_BUCKET, {
     
 module.exports = (app) => {
 
+        /* GET home page. */
+    app.get('/services', (req, res) => {
+        const page = req.query.page || 1
+
+        Service.paginate({}, {page: page}).then((results) => {
+            if (req.header('content-type') == 'application/json') {
+                return res.json({ services: services, categories: categories });
+            } else {
+                return res.render('services-index', { services: results.docs, pagesCount: results.pages, currentPage: page });
+            }
+
+        });
+    });
+  
     // new service
     app.get('/services/new', (req, res) => {
         res.render('services-new');
